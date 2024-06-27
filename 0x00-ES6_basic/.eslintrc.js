0665e42dbdce3df73d3769c683ea4d641f57dab6
DaBetterman <1ebrahimr@gmail.com>
2024-06-27 20:23:42 +0200

    removed sensitive info

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+};
